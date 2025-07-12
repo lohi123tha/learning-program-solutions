b0bf1df7b1ece3e0d6d3a71312a9bf2118d0595d
lohi123tha <167747366+lohi123tha@users.noreply.github.com>
2025-07-12 20:43:57 +0530

    Create com.cognizant.springlearn.config.SecurityConfig.java

---
+++ week4/handson/JWT/com.cognizant.springlearn.config.SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.cognizant.springlearn.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable() // disable CSRF
+            .authorizeRequests()
+                .anyRequest().permitAll(); // allow all
+    }
+}
