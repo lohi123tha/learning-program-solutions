29fd9594fe905375ac2d41d69869f09ed42bd15c
lohi123tha <167747366+lohi123tha@users.noreply.github.com>
2025-07-12 20:41:40 +0530

    Create com.cognizant.springlearn.util.JwtUtil.java

---
+++ week4/handson/JWT/com.cognizant.springlearn.util.JwtUtil.java
@@ -0,0 +1,22 @@
+package com.cognizant.springlearn.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET_KEY = "secret"; // ideally stored in env
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hr
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
