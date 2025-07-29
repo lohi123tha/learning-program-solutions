6bfbadeafcdae593fd3395710ce9cccd013eedee
lohi123tha <167747366+lohi123tha@users.noreply.github.com>
2025-07-29 11:27:30 +0530

    Create contact.js

---
+++ week6/handson/reactjs/2/contact.js
@@ -0,0 +1,12 @@
+// src/Components/Contact.js
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h3>Welcome to the Contact Page of Student Management Portal</h3>
+    </div>
+  );
+}
+
+export default Contact;
