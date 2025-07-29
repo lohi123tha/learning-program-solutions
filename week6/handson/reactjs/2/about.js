914d19f83671c44077980748af2006352f255196
lohi123tha <167747366+lohi123tha@users.noreply.github.com>
2025-07-29 11:27:15 +0530

    Create about.js

---
+++ week6/handson/reactjs/2/about.js
@@ -0,0 +1,12 @@
+// src/Components/About.js
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h3>Welcome to the About Page of Student Management Portal</h3>
+    </div>
+  );
+}
+
+export default About;
